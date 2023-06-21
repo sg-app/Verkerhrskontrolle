ec139946301851bb37928fcae6ced30034503902
LukasLindheim <luke.lindheim@gmail.com>
2023-06-21 11:20:41 +0200

    Implement GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync Implement GetFahrzeugtypErlaubnisByFuehrerscheinnummerAsync

--- Services/HalterAbfrageService.cs
+++ Services/HalterAbfrageService.cs
@@ -1,18 +1,82 @@
-﻿using Verkehrskontrolle.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Verkehrskontrolle.Data;
+using Verkehrskontrolle.Interfaces;
 using Verkehrskontrolle.Models;
 
 namespace Verkehrskontrolle.Services
 {
     public class HalterAbfrageService : IHalterAbfrageService
     {
-        public Task<bool> GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync(string kennzeichen, int fuehrerscheinId)
-        {
-            throw new NotImplementedException();
+        private readonly VerkehrskontrolleDbContext _context;
+
+        public HalterAbfrageService(VerkehrskontrolleDbContext context) { _context = context; }
+        /// <summary>
+        /// Prüft ob der Halter der FührerscheinId das Auto mit dem angegebenen Kennzeichen fahren darf.
+        /// </summary>
+        /// <param name="kennzeichen"></param>
+        /// <param name="fuehrerscheinId"></param>
+        /// <returns></returns>
+        public async Task<bool> GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync(string kennzeichen, int fuehrerscheinId)
+        {        
+            var fahrzeug = await _context.Fahrzeuge.FirstOrDefaultAsync(f => f.Kennzeichen == kennzeichen);
+            var führerschein = _context.Fuehrerscheine.FirstOrDefault(x => x.Id == fuehrerscheinId);
+
+            if(führerschein == null || fahrzeug == null)
+            {
+                return false;
+            }
+
+            if (!führerschein.LKWErlaubnis)
+            {
+                if (!fahrzeug.Fahrzeugtyp.Contains("LKW"))
+                {
+                    return false;
+                }
+            }
+
+            if (!führerschein.PKWErlaublnis)
+            {
+                if (!fahrzeug.Fahrzeugtyp.Contains("PKW"))
+                {
+                    return false;
+                }
+            }
+            if (!führerschein.AnhängerErlaubnis)
+            {
+                if (!fahrzeug.Fahrzeugtyp.Contains("Anhänger"))
+                {
+                    return false;
+                }
+            }
+            return true;            
         }
 
-        public Task<ICollection<string>> GetFahrzeugtypErlaubnisByFuehrerscheinnummerAsync(int fuehrerscheinId)
+        /// <summary>
+        /// Erhält eine Liste der erlaubten Fahrzeugtypen für einen Führerschein
+        /// </summary>
+        /// <param name="fuehrerscheinId"></param>
+        /// <returns></returns>
+        public async Task<ICollection<string>> GetFahrzeugtypErlaubnisByFuehrerscheinnummerAsync(int fuehrerscheinId)
         {
-            throw new NotImplementedException();
+            var führerschein = _context.Fuehrerscheine.FirstOrDefault(x => x.Id == fuehrerscheinId);
+            var erlaubnisListe = new List<string>();
+
+            if (führerschein.AnhängerErlaubnis)
+            {
+                erlaubnisListe.Add("Anhänger");
+            }
+
+            if (führerschein.LKWErlaubnis)
+            {
+                erlaubnisListe.Add("LKW");
+            }
+
+            if (führerschein.PKWErlaublnis)
+            {
+                erlaubnisListe.Add("PKW");
+            }
+
+            return erlaubnisListe;
         }
 
         public Task<bool> GetFührerscheinIstGültigByFuehrerscheinnummerAsync(int fuehrerscheinId)
