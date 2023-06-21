2d135a9149b8b7515842def7378bb3e5242c29cf
Georg Schreiner <uid15076@contiwan.com>
2023-06-21 10:32:42 +0200

    Added serviceclass

---
+++ Services/HalterAbfrageService.cs
@@ -0,0 +1,33 @@
+﻿using Verkehrskontrolle.Interfaces;
+using Verkehrskontrolle.Models;
+
+namespace Verkehrskontrolle.Services
+{
+    public class HalterAbfrageService : IHalterAbfrageService
+    {
+        public Task<bool> GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync(string kennzeichen, int fuehrerscheinId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<string>> GetFahrzeugtypErlaubnisByFuehrerscheinnummerAsync(int fuehrerscheinId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> GetFührerscheinIstGültigByFuehrerscheinnummerAsync(int fuehrerscheinId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Halter> GetHalterDetailsByKennzeichenAsync(string kennzeichen)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Halter> GetHalterDetailsByNameUndGeburtsdatumAsync(string vorname, string nachname, DateTime geburtsdatum)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
