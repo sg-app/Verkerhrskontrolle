b2417eb5f8412fe03a8fb714b118cc28a8262d49
Onur Satir <uidp3714@contiwan.com>
2023-06-21 09:42:00 +0200

    Änderung Klassen

--- Führerschein.cs
+++ Führerschein.cs
@@ -2,5 +2,16 @@
 {
     public class Führerschein
     {
+        public int Id { get; set; }
+       
+        public DateTime Gültigkeit { get; set; }
+
+        public string PKWErlaublnis { get; set; }
+
+        public string AnhängerErlaubnis { get; set; }
+
+        public string LKWErlaubnis { get; set; }
+
+
     }
 }
--- Halter.cs
+++ Halter.cs
@@ -2,5 +2,20 @@
 {
     public class Halter
     {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime Geburtdatum { get; set;}
+        public string Straße { get; set; }
+        public int Hausnummer { get; set; }
+        public int Postleitzahl { get; set; }
+        public string Ort { get; set;}
+        public int FührerscheinNummer { get; set; }
+
+
+
+
+
+
     }
 }
