67e7c6c4381983158d6fb29de3580893b13e9927
Georg Schreiner <uid15076@contiwan.com>
2023-06-21 11:26:54 +0200

    added halterabfrage controller

---
+++ Controllers/HalterAbfrageController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Verkehrskontrolle.Interfaces;
+using Verkehrskontrolle.Models;
+
+namespace Verkehrskontrolle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HalterAbfrageController : ControllerBase
+    {
+        private readonly IHalterAbfrageService _service;
+
+        public HalterAbfrageController(IHalterAbfrageService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{kennzeichen}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<Halter>> GetByKennzeichen(string kennzeichen)
+        {
+            var found = await _service.GetHalterDetailsByKennzeichenAsync(kennzeichen);
+            if(found == null)
+                return NotFound();
+
+            return Ok(found);
+        }
+
+        [HttpGet("ByNameUndGeburtstag")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<Halter>> GetByNameUndGeburtstag(string vorname, string nachname, DateTime geburtsdatum)
+        {
+            var found = await _service.GetHalterDetailsByNameUndGeburtsdatumAsync(vorname, nachname, geburtsdatum);
+            if (found == null)
+                return NotFound();
+
+            return Ok(found);
+        }
+
+        [HttpGet("FueherscheinGueltig")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<Halter>> GetFuehrerscheinGueltig(int fueherscheinnummer)
+        {
+            var gueltig = await _service.GetFührerscheinIstGültigByFuehrerscheinnummerAsync(fueherscheinnummer);
+            return Ok(gueltig);
+        }
+
+        [HttpGet("Fahrerlaubnis")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<Halter>> GetFahrzeugtypErlaubt(string kennzeichen, int fueherscheinnummer)
+        {
+            var erlaubnis = await _service.GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync(kennzeichen, fueherscheinnummer);
+            return Ok(erlaubnis);
+        }
+    }
+}
