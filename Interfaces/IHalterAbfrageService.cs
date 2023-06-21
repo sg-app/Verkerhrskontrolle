5cafb37c791d33d2195cec525d19f6923597f5df
Georg Schreiner <uid15076@contiwan.com>
2023-06-21 10:06:22 +0200

    define interface

---
+++ Interfaces/IHalterAbfrageService.cs
@@ -0,0 +1,23 @@
+﻿using Verkehrskontrolle.Models;
+
+namespace Verkehrskontrolle.Interfaces
+{
+    public interface IHalterAbfrageService
+    {
+
+        Task<Halter> GetHalterDetailsByKennzeichenAsync(string kennzeichen);
+        Task<Halter> GetHalterDetailsByNameUndGeburtsdatumAsync(string vorname, string nachname, DateTime geburtsdatum);
+
+        Task<bool> GetFührerscheinIstGültigByFuehrerscheinnummerAsync(int fuehrerscheinId);
+       
+        /// <summary>
+        /// Abfrage was Halter alles fahren darf.
+        /// </summary>
+        /// <param name="fuehrerscheinId"></param>
+        /// <returns>Liste der erlaubten Farzeugtypen</returns>
+        Task<ICollection<string>> GetFahrzeugtypErlaubnisByFuehrerscheinnummerAsync(int fuehrerscheinId);
+
+        Task<bool> GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync(string kennzeichen, int fuehrerscheinId);
+
+    }
+}
