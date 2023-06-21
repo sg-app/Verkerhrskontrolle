0062dae5b597f34d05bdd05c1ce24886b21fba88
Georg Schreiner <uid15076@contiwan.com>
2023-06-21 13:21:55 +0200

    fix solution

---
+++ VerkehrskontrolleTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace VerkehrskontrolleTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
