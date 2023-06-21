df01119a4be0cfdee93a6e1ca06b41f52747f504
Onur Satir <uidp3714@contiwan.com>
2023-06-21 13:27:34 +0200

    TEst GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync_ShouldBeTrue

--- VerkehrskontrolleTests/Tests.cs
+++ VerkehrskontrolleTests/Tests.cs
@@ -1,16 +1,36 @@
+using Verkehrskontrolle.Controllers;
+using Verkehrskontrolle.Interfaces;
+
 namespace VerkehrskontrolleTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+
+
+
 
         [Test]
-        public void Test1()
+        public void GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync_ShouldBeTrue()
         {
-            Assert.Pass();
+            //Arrange
+
+
+
+            var mok = new Mock<IHalterAbfrageService>();
+            var kennzeichen = "R-A-1234";
+
+            mok.Setup(m => m.GetFahrerlaubnisByKennzeichenUndFuehrerscheinnummerAsync(kennzeichen, 1)).ReturnsAsync(true);
+
+
+            //Act
+            var respones = new HalterAbfrageController(mok.Object);
+
+            var result = respones.GetFahrzeugtypErlaubt(kennzeichen, 1);
+
+
+            //Assert
+            Assert.That(result, Is.Not.Null);
+
         }
     }
 }
