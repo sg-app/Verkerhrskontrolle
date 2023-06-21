1e133cd90e95dfaef0220818dbafa8ae7bcd375f
Georg Schreiner <uid15076@contiwan.com>
2023-06-21 16:04:26 +0200

    adding halter controller tests

---
+++ VerkehrskontrolleTests/Controllers/HalterControllerTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Moq.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verkehrskontrolle.Controllers;
+using Verkehrskontrolle.Data;
+using Verkehrskontrolle.Models;
+
+namespace VerkehrskontrolleTests.Controllers
+{
+    public class HalterControllerTests
+    {
+        [Test]
+        public async Task GetAll_ShouldReturnsListOfHalter()
+        {
+            // Arrange
+            var list = Helpers.HelperData.HalterList();
+            
+            var ctxMock = new Mock<VerkehrskontrolleDbContext>();
+            ctxMock.Setup(f => f.Halter).ReturnsDbSet(list);
+
+            var controller = new HalterController(ctxMock.Object);
+            
+            // Act
+            var actual = await controller.GetAll();
+            
+            // Assert
+            actual.Value.Should().BeEquivalentTo(list);
+        }
+
+        [Test]
+        public async Task Get_ShouldReturnsOneHalter()
+        {
+            // Arrange
+            var list = Helpers.HelperData.HalterList();
+
+            var halterSetMock = new Mock<DbSet<Halter>>();
+            halterSetMock.Setup(x => x.FindAsync(It.IsAny<int>())).ReturnsAsync(list[1]);
+           
+            var ctxMock = new Mock<VerkehrskontrolleDbContext>();
+            ctxMock.SetupGet(f => f.Halter).Returns(halterSetMock.Object);
+
+            var controller = new HalterController(ctxMock.Object);
+
+            // Act
+            var actual = await controller.Get(1);
+
+            // Assert
+            ((OkObjectResult)actual.Result).Value.Should().Be(list[1]);
+        }
+
+        [Test]
+        public async Task Get_ShouldReturnsNoContent()
+        {
+            // Arrange
+            var list = Helpers.HelperData.HalterList();
+
+            var ctxMock = new Mock<VerkehrskontrolleDbContext>();
+            ctxMock.SetupGet(f => f.Halter).ReturnsDbSet(list);
+
+            var controller = new HalterController(ctxMock.Object);
+
+            // Act
+            var actual = await controller.Get(-1);
+
+            // Assert
+            actual.Result.Should().BeOfType<NoContentResult>();
+        }
+
+
+    }
+}
---
+++ VerkehrskontrolleTests/Helpers/HelperData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verkehrskontrolle.Models;
+
+namespace VerkehrskontrolleTests.Helpers
+{
+    public static class HelperData
+    {
+        public static List<Halter> HalterList()
+        {
+            List<Halter> list = new();
+            var fsCount = FuehrerscheinList().Count;
+            for (int i = 0; i < 10; i++)
+            {
+                var idx = new Random().Next(fsCount);
+                list.Add(new Halter()
+                {
+                    Id = i,
+                    Vorname = $"Vorname {i}",
+                    Nachname = $"Nachname {i}",
+                    Geburtsdatum = DateTime.Today.AddDays(-new Random().Next(8000)),
+                    Straße = $"Straße {i}",
+                    Hausnummer = new Random().Next(100),
+                    Postleitzahl = new Random().Next(10000,99999),
+                    Ort = $"Ort {i}",
+                    FührerscheinId = FuehrerscheinList()[idx].Id,
+                });
+            }
+            return list;
+        }
+
+        public static List<Führerschein> FuehrerscheinList()
+        {
+            List<Führerschein> list = new();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new Führerschein()
+                {
+                    Id = i,
+                    Gültigkeit = DateTime.Today.AddDays(new Random().Next(8000)),
+                    PKWErlaublnis = new Random().Next(2) == 0,
+                    AnhängerErlaubnis = new Random().Next(2) == 0,
+                    LKWErlaubnis = new Random().Next(2) == 0,
+                });
+            }
+            return list;
+        }
+    }
+}
