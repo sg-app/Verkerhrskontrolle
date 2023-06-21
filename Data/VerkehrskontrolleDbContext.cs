9819990cc9748613b3211e411c67af5728987dbe
Georg Schreiner <uid15076@contiwan.com>
2023-06-21 10:06:13 +0200

    added new dbcontext

---
+++ Data/VerkehrskontrolleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Verkehrskontrolle.Models;
+
+namespace Verkehrskontrolle.Data
+{
+    public class VerkehrskontrolleDbContext : DbContext
+    {
+
+        public DbSet<Fahrzeug> Fahrzeuge { get; set; }
+        public DbSet<Halter> Halter { get; set; }
+        public DbSet<Führerschein> Fuehrerscheine { get; set; }
+
+        public VerkehrskontrolleDbContext(DbContextOptions options): base(options)
+        {
+            
+        }
+    }
+}
